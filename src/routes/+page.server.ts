2d35a242fe546859b4b37dd007505352f56773de
NMacOmega <nathaniel.maciver@gmail.com>
2023-08-04 11:25:41 -0400

    updated variables using gh client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import { error, type Load } from '@sveltejs/kit';
 
-import { MODEL_URL } from '$env/static/private';
+// import { MODEL_URL } from '$env/static/private';
 
 export const load: Load = async () => {
     return {
-        modelURL: MODEL_URL
+        modelURL: `test`
     };
 };
